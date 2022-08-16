d8f52a3436a9a4f6541f1247a379f7ce180986fa
Florian Begusch <florian.begusch@gmail.com>
2022-08-16 05:46:42 +0000

    Add archlinux-yay-remove-package-info.py

---
+++ archlinux-yay-remove-package-info.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+with open(sys.argv[1], 'r') as f:
+    content = f.read()
+
+oneline_matches = 'pkg(ver|rel)=[^\n]+'
+multiline_matches = '(sha256|md5)sums[^\s]*=\([^)]+\)'
+regexp = re.compile('^(' + oneline_matches + '|' + multiline_matches + ')', re.MULTILINE)
+
+print(regexp.sub('', content))
+
