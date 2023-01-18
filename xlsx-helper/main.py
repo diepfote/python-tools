b41c2ad9bc41d1983ff8894805ac72abdea69bd6
Florian Begusch <florian.begusch@gmail.com>
2023-01-18 06:02:32 +0000

    xslx-helper: print all entries if no name given

--- xlsx-helper/main.py
+++ xlsx-helper/main.py
@@ -3,7 +3,11 @@ import pandas as pd
 import sys
 
 df = pd.read_excel(sys.argv[1])
-person_to_filter_for = sys.argv[2]
+
+if len(sys.argv) > 2:
+    person_to_filter_for = sys.argv[2]
+else:
+    person_to_filter_for = None
 # breakpoint()
 
 less_data = df.loc[:, ['Bereitschaft', 'Column1', 'Column12']]
@@ -16,12 +20,20 @@ for _, row in less_data.iterrows():
     end = row[2]
     # add a day since otherwise we do not account for the entirety of the last day
     end += pd.DateOffset(1)
-    if name != person_to_filter_for:
+
+    event = Event()
+
+    if not person_to_filter_for:
+        event.name = name
+    elif name != person_to_filter_for:
         continue
+    elif name == person_to_filter_for:
+        event.name = 'Bereitschaft'
+    else:
+        raise Exception('Unexpected case')
 
     print(f'{name}\t\t{start}\t{end}')
-    event = Event()
-    event.name = 'Bereitschaft'
+
     event.begin = start
     event.end = end
     calendar.events.add(event)
