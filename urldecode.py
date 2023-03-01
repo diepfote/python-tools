229855b47e5154b3c4955c52edc4cc545ca73424
Florian Begusch <florian.begusch@gmail.com>
2023-03-01 15:10:26 +0000

    Update `urldecode`

--- urldecode.py
+++ urldecode.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 from urllib.parse import unquote
 
-print(unquote(sys.argv[1]))
+if len(sys.argv) > 2 and sys.argv[1] == '--oauth2':
+    url = sys.argv[2]
+    url = unquote(url)
+    url = url.split('&g0.tab=1')[0]
+    url = re.sub('/oauth2/[^/]+/', '/', url)
+else:
+    url = sys.argv[1]
+    url = unquote(url)
 
+print(url)
