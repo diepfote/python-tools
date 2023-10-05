9c13477200e1db17c8768a328e2699437baf856f
Florian Begusch <florian.begusch@gmail.com>
2023-10-05 16:29:58 +0000

    `regex-sub`: Fix help output & add read string to replace from `stdin`

--- regex-substitute.py
+++ regex-substitute.py
@@ -17,22 +17,35 @@ def _help():
     usage_info = f'''{red}USAGE{no_color}: {executable_name} ORIGINAL PATTERN REPLACEMENT
 
 {purple}Examples{no_color}:
-{yellow}$ regex-sub '__:45:fc:5d:c3:55:48:89:e5:89:7d:fc' '([0-9a-f_]{2}):([0-9a-f_]{2}):?' '\1\2 '{no_color}
+$ {executable_name} {yellow}'__:45:fc:5d:c3:55:48:89:e5:89:7d:fc' '([0-9a-f_]{{2}}):([0-9a-f_]{{2}}):?' '\\1 \\2'{no_color}
+original='__:45:fc:5d:c3:55:48:89:e5:89:7d:fc'
+pattern='([0-9a-f_]{{2}}):([0-9a-f_]{{2}}):?'
+replacement='\\1 \\2'
+{green}__ 45fc 5dc3 5548 89e5 897d fc{no_color}
+
+$ {executable_name} {yellow}'__:45:fc:5d:c3:55:48:89:e5:89:7d:fc' '([0-9a-f_]{{2}}):([0-9a-f_]{{2}}):?' '\\1 \\2'{no_color}  2>/dev/null
+{green}__ 45fc 5dc3 5548 89e5 897d fc{no_color}
+
+$ {yellow}echo -n '__:45:fc:5d:c3:55:48:89:e5:89:7d:fc'  | regex-sub '([0-9a-f_]{{2}}):([0-9a-f_]{{2}}):?' '\\1 \\2'{no_color}
 original='__:45:fc:5d:c3:55:48:89:e5:89:7d:fc'
 pattern='([0-9a-f_]{2}):([0-9a-f_]{2}):?'
-replacement='\\1\\2 '
-__45 fc5d c355 4889 e589 7dfc
-/etc
-{yellow}$ regex-sub '__:45:fc:5d:c3:55:48:89:e5:89:7d:fc' '([0-9a-f_]{2}):([0-9a-f_]{2}):?' '\1\2 '  2>/dev/null{no_color}
-__45 fc5d c355 4889 e589 7dfc
-'''
+replacement='\\1 \\2'
+{green}__ 45fc 5dc3 5548 89e5 897d fc{no_color}
+
+$ {yellow}echo -en '__:45:fc:5d:c3:55:48:89:e5:89:7d:fc\\n__:45:fc:5d'  | regex-sub '([0-9a-f_]{{2}}):([0-9a-f_]{{2}}):?' '\\1 \\2'{no_color}
+original='__:45:fc:5d:c3:55:48:89:e5:89:7d:fc\\n__:45:fc:5d'
+pattern='([0-9a-f_]{{2}}):([0-9a-f_]{{2}}):?'
+replacement='\\1 \\2'
+{green}__ 45fc 5dc3 5548 89e5 897d fc
+__ 45fc 5d{no_color}'''
+
     print(usage_info, file=sys.stderr)
 
 
 num_args = len(sys.argv)
-required_num_args = 4
+required_num_args = 3
 
-if num_args < required_num_args or num_args > required_num_args:
+if num_args < required_num_args:
     _help()
     exit(1)
 first_arg = sys.argv[1]
@@ -40,10 +53,14 @@ if first_arg == '-h' or first_arg == '--help':
     _help()
     exit(0)
 
-original = first_arg
-pattern = sys.argv[2]
-replacement = sys.argv[3]
-
+if num_args > required_num_args:
+    original = first_arg
+    pattern = sys.argv[2]
+    replacement = sys.argv[3]
+else:
+    pattern = first_arg
+    replacement = sys.argv[2]
+    original = sys.stdin.read()
 
 print(f'{original=}', file=sys.stderr)
 print(f'{pattern=}', file=sys.stderr)
