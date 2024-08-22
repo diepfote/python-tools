68700317e7471b9f66e2e8b541b4c0c27aecb041
Florian Sorko <florian.begusch@gmail.com>
2024-08-22 16:35:55 +0000

    `regex-sub`: mimic `test-sed`

--- regex-substitute.py
+++ regex-substitute.py
@@ -2,8 +2,8 @@
 
 import os
 import re
+import subprocess
 import sys
-
 
 executable_name = os.path.basename(sys.argv[0])
 green = os.environ.get('GREEN', '')
@@ -37,10 +37,29 @@ original='__:45:fc:5d:c3:55:48:89:e5:89:7d:fc\\n__:45:fc:5d'
 pattern='([0-9a-f_]{{2}}):([0-9a-f_]{{2}}):?'
 replacement='\\1 \\2'
 {green}__ 45fc 5dc3 5548 89e5 897d fc
-__ 45fc 5d{no_color}'''
+__ 45fc 5d{no_color}
+
+run with: {purple}SILENT=true DEBUG=true{no_color} to mimic {yellow}test-sed{no_color}
+e.g.: SILENT=true DEBUG=true regex-sub 'pattern' 'replacement' < filename'''
 
     print(usage_info, file=sys.stderr)
 
+
+def debug(edited, original):
+    orig = '/tmp/regex-sub-original'
+    edit = '/tmp/regex-sub-edited'
+    with open(orig, 'w') as f:
+        f.write(original)
+
+    with open(edit, 'w') as f:
+        f.write(edited)
+
+
+    command = ['git', '-c', 'color.status=always', 'diff', '--word-diff', '--no-index', orig, edit]
+    subprocess.run(command, stdout=sys.stdout, stderr=sys.stderr)
+
+    os.remove(orig)
+    os.remove(edit)
 
 num_args = len(sys.argv)
 required_num_args = 3
@@ -62,8 +81,13 @@ else:
     replacement = sys.argv[2]
     original = sys.stdin.read()
 
-print(f'{original=}', file=sys.stderr)
-print(f'{pattern=}', file=sys.stderr)
-print(f'{replacement=}', file=sys.stderr)
+if not os.environ.get('SILENT', None):
+    print(f'{original=}', file=sys.stderr)
+    print(f'{pattern=}', file=sys.stderr)
+    print(f'{replacement=}', file=sys.stderr)
 
-print(re.sub(pattern, replacement, original))
+edited = re.sub(pattern, replacement, original)
+if os.environ.get('DEBUG', None):
+    debug(edited, original)
+else:
+    print(edited)
