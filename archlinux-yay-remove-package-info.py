efac297f700d5b1801a5f54db7955e7cf9d5a5e1
Florian Sorko <florian.begusch@gmail.com>
2025-09-25 03:20:33 +0000

    `archlinux-yay-remove-package-info.py`: exclude all sha hashes

--- archlinux-yay-remove-package-info.py
+++ archlinux-yay-remove-package-info.py
@@ -7,7 +7,7 @@ with open(sys.argv[1], 'r') as f:
     content = f.read()
 
 oneline_matches = 'pkg(ver|rel)=[^\n]+'
-multiline_matches = '(sha256|md5)sums[^\\s]*=\\([^)]+\\)'
+multiline_matches = '(sha[0-9]+|md5)sums[^\\s]*=\\([^)]+\\)'
 regexp = re.compile('^(' + oneline_matches + '|' + multiline_matches + ')', re.MULTILINE)
 
 print(regexp.sub('', content))
